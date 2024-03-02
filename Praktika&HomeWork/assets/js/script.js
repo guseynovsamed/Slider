36adb1ea1013794242ec77ed6bcac197254dacae
guseynovsamed <samadaghash@code.edu.az>
2024-03-03 03:23:06 +0400

    Slider

---
+++ Praktika&HomeWork/assets/js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// file update
+//#region
+
+// let fileInput = document.querySelector(".file-input");
+// let tableBody = document.querySelector("tbody");
+
+// fileInput.addEventListener("change", function (e) {
+//   for (let i = 0; i < e.target.files.length; i++) {
+//     const fileReader = new FileReader();
+//     fileReader.readAsDataURL(e.target.files[i]);
+//     fileReader.onload = (event) => {
+//       let elem = "";
+//       elem += `<tr>
+//             <td><img src="${
+//               event.target.result
+//             }" style="width: 100px; height: 100px" alt="" /></td>
+//             <td>${e.target.files[i].name}</td>
+//             <td>${e.target.files[i].size / 1024}</td>
+//             <td>
+//             <a class="btn btn-primary">Edit</a>
+//             <a class="btn btn-warning">Detail</a>
+//             <a class="btn btn-danger">Delete</a>
+//           </td>
+//         </tr>`;
+
+//       tableBody.innerHTML += elem;
+//     };
+//   }
+// });
+//#endregion
+
+//HOME WORK
+//#region
+
+// let left = document.querySelector(".slider .icons .left");
+// let right = document.querySelector(".slider .icons .right");
+
+// right.addEventListener("click", function () {
+//   let next = document.querySelector(".next");
+//   next.classList.remove("next");
+
+//   if (next.nextElementSibling != null) {
+//     next.nextElementSibling.classList.add("next");
+//   } else {
+//     this.parentNode.nextElementSibling.firstElementChild.classList.add("next");
+//   }
+// });
+
+// left.addEventListener("click", function () {
+//   let next = document.querySelector(".next");
+//   next.classList.remove("next");
+//   if (next.previousElementSibling != null) {
+//     next.previousElementSibling.classList.add("next");
+//   } else {
+//     this.parentNode.nextElementSibling.lastElementChild.classList.add("next");
+//   }
+// });
+
+//#endregion
+
+
+
